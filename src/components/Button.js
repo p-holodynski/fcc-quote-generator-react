06e6fde9f5833fa145b5c22ac7ccb2472a5921ea
Patryk Holodynski <p.holodynski@gmail.com>
2019-04-09 17:04:41 +0100

    test

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // Presentational component, stateless functional component
+// custom wrapper around the HTML button element
 const Button = ({ buttonDisplayName, clickHandler }) => (
     <button onClick={clickHandler}>{buttonDisplayName}</button>
 );
